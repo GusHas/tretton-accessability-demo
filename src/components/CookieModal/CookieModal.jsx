bb7f2c375788d2501de4938e82a5560cd24b9fca
Gustav Hässler <e-gustav.hassler@smart.com>
2024-11-07 15:55:41 +0100

    bottomlinks optimisation

--- src/components/CookieModal/CookieModal.jsx
+++ src/components/CookieModal/CookieModal.jsx
@@ -12,6 +12,17 @@ export const CookieModal = ({ setIsOpen }) => {
     );
   };
 
+  const BottomLink = ({ label, failText, hasBreak }) => {
+    return (
+      <>
+        <a role="link" tabIndex={0} onClick={() => failState(failText)}>
+          {label}
+        </a>
+        {hasBreak && " /"}
+      </>
+    );
+  };
+
   return (
     <div className="cookieModal">
       <div
@@ -46,44 +57,33 @@ export const CookieModal = ({ setIsOpen }) => {
           </button>
         </div>
         <div className="bottomContent">
-          <a
-            role="link"
-            tabIndex={0}
-            onClick={() => failState("Inget du kan anpassa just nu")}
-          >
-            Anpassa ditt val
-          </a>{" "}
-          /
-          <a
-            role="link"
-            tabIndex={0}
-            onClick={() =>
-              failState(
-                "Lyckligtvis behöver du inte bry dig om kakor på det här demot."
-              )
-            }
-          >
-            Cookies
-          </a>{" "}
-          /
-          <a
-            role="link"
-            tabIndex={0}
-            onClick={() => failState("Finns inget och läsa mer kring just nu")}
-          >
-            Läs mer
-          </a>{" "}
-          /
-          <a
-            role="link"
-            tabIndex={0}
-            onClick={() => failState('Här har vi ingen användning "lagen".')}
-          >
-            Rättsligt meddelande
-          </a>
+          <BottomLink
+            failText="Inget du kan anpassa just nu"
+            label="Anpassa ditt val"
+            hasBreak
+          />
+          <BottomLink
+            failText="Lyckligtvis behöver du inte bry dig om kakor på det här demot."
+            label="Cookies"
+            hasBreak
+          />
+          <BottomLink
+            failText="Finns inget och läsa mer kring just nu"
+            label="Läs mer"
+            hasBreak
+          />
+          <BottomLink
+            failText='Här har vi ingen användning för "lagen".'
+            label="Rättsligt meddelande"
+          />
         </div>
-        <div className="closeButton" role="button" onClick={setIsOpen} tabIndex={0}>
-          <X className="icon" alt="nedstängningsknapp"/>
+        <div
+          className="closeButton"
+          role="button"
+          onClick={setIsOpen}
+          tabIndex={0}
+        >
+          <X className="icon" alt="nedstängningsknapp" />
         </div>
       </div>
     </div>
